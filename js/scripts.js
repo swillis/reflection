d174ca2803ca18ca37c0bc659c8b0a293d1f64d6
Sam Willis <sam.willis@event.ly>
2013-10-06 23:22:15 +0100

    Edits to slider and mobile site

--- js/scripts.js
+++ js/scripts.js
@@ -115,29 +115,6 @@ jQuery(document).ready(function() {
 	});
 	
 	/*-----------------------------------------------------------------------------------*/
-	/*	Studio
-	/*-----------------------------------------------------------------------------------*/
-	
-	/* Initialize Slider */	
-	var swiper = jQuery('#swiper').swiper({
-		loop:true,
-		grabCursor: true,
-		autoPlay: 4000
-	});
-	
-	/* On Load swiper height should adjust to img size */
-	jQuery('.swiper-slide img').load(function() {
-		jQuery('#swiper').height(jQuery('.swiper-slide img').height());
-		jQuery('.swiper-wrapper').height(jQuery('.swiper-slide img').height());
-	});
-	
-	/* On Resize swiper height should adjust to img size */
-	jQuery(window).resize(function() {
-		jQuery('#swiper').height(jQuery('.swiper-slide img').height());
-		jQuery('.swiper-wrapper').height(jQuery('.swiper-slide img').height());
-	});
-	
-	/*-----------------------------------------------------------------------------------*/
 	/*	Services
 	/*-----------------------------------------------------------------------------------*/
 	
